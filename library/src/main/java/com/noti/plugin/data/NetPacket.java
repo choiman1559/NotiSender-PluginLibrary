b2c1c5b60eebcd0baaeb091593bcbaaff9ba5a92
cuj1559 <cuj1559@gmail.com>
2023-11-25 18:57:22 +0900

    Fixed Serialize Object local class incompatible

--- library/src/main/java/com/noti/plugin/data/NetPacket.java
+++ library/src/main/java/com/noti/plugin/data/NetPacket.java
@@ -1,15 +1,29 @@
 package com.noti.plugin.data;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings("unused")
 public class NetPacket implements Serializable {
+    private static final long serialVersionUID = 1234L;
     String[] keyList;
     String [] valueList;
+    int length;
 
     private NetPacket(int length) {
+        this.length = length;
         keyList = new String[length];
         valueList = new String[length];
+    }
+
+    public Map<String, String> build() {
+        Map<String, String> newMap = new HashMap<>();
+        for(int i = 0; i < length; i++) {
+            newMap.put(keyList[i], valueList[i]);
+        }
+
+        return newMap;
     }
 
     public static NetPacket parseFrom(Map<String, String> map) {
